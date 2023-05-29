a8948218415630382ac062b921b35e027e3dac01
ejcanoy <euan.james.canoy@gmail.com>
2023-05-28 19:55:48 -0700

    created header

--- src/index.js
+++ src/index.js
@@ -1,31 +1,67 @@
-import './style.css'
+import "./style.css";
 
-// function component() {
-//     const element = document.createElement('div');
+function body() {
+  document.body.appendChild(header());
+  return;
+}
+
+function header() {
+  const headerElement = document.createElement("header");
+  // create title
+  const title = document.createElement("div");
+
+  title.innerHTML = "Daizha Espiritu";
+  title.classList.add("title");
   
-//     // Lodash, currently included via a script, is required for this line to work
-//     element.innerHTML = 'Daizha Espiritu';
-//     element.classList.add('hello');
-//     return element;
-//   }
+  // create nav
+  const navElement = document.createElement("nav");
+  const olElement = document.createElement("ol");
+
+  const tabData = [
+    { href: "#about-section", text: "ABOUT" },
+    { href: "#work-section", text: "WORK" },
+    { href: "#contact-section", text: "CONTACT" }
+  ];
   
-  // document.body.appendChild(component());
-document.addEventListener('DOMContentLoaded', function() {
+  for (let i = 0; i < tabData.length; i++) {
+    const tab = tabData[i];
+  
+    const liElement = document.createElement("li");
+    liElement.classList.add("nav-tab");
+  
+    const aElement = document.createElement("a");
+    aElement.setAttribute("href", tab.href);
+    aElement.innerText = tab.text;
+  
+    liElement.appendChild(aElement);
+    olElement.appendChild(liElement);
+  }
+
+  navElement.appendChild(olElement);
+
+  headerElement.appendChild(title);
+  headerElement.appendChild(navElement);
+  return headerElement;
+}
+
+body();
+
+document.addEventListener("DOMContentLoaded", function () {
   // Get all the navigation links
-  const navLinks = document.querySelectorAll('.nav-tab a');
+  const navLinks = document.querySelectorAll(".nav-tab a");
 
   // Iterate over each navigation link
-  navLinks.forEach(function(link) {
+  navLinks.forEach(function (link) {
     // Add a click event listener to each link
-    link.addEventListener('click', function(event) {
+    link.addEventListener("click", function (event) {
       event.preventDefault(); // Prevent default link behavior
 
       // Get the target section ID from the link's href attribute
-      const targetId = link.getAttribute('href');
+      const targetId = link.getAttribute("href");
 
       // Scroll to the target section smoothly
       document.querySelector(targetId).scrollIntoView({
-        behavior: 'smooth'
+        behavior: "smooth",
       });
     });
   });
